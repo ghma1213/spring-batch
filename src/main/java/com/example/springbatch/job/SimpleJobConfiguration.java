fc9ae331db9f1487048bd61ad6dd4ca8e33de46d
kyunghwanma <makh1213@naver.com>
2022-12-13 21:23:31 +0900

    Spring Batch Scope & Job Parameter

--- src/main/java/com/example/springbatch/job/SimpleJobConfiguration.java
+++ src/main/java/com/example/springbatch/job/SimpleJobConfiguration.java
@@ -5,8 +5,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -24,19 +25,41 @@ public class SimpleJobConfiguration {
         return jobBuilderFactory.get("simpleJob")
 //                .start(simpleStep1())
                 .start(simpleStep1(null))
+                .next(simpleStep2())
+                .build();
+    }
+
+    private final SimpleJobTasklet tasklet1;
+
+//    @Bean
+//    @JobScope
+    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return stepBuilderFactory.get("simpleStep1")
+//                .tasklet((contribution, chunkContext) -> {
+//                    log.info("Step1!!!!");
+//                    log.info("requestDate = {}", requestDate);
+//                    return RepeatStatus.FINISHED;
+//                })
+                .tasklet(tasklet1)
+                .build();
+    }
+
+
+    @Bean
+    public Step simpleStep2() {
+        return stepBuilderFactory.get("simpleStep2")
+                .tasklet(simpleStep2Tasklet(null))
                 .build();
     }
 
     @Bean
-    @JobScope
-    public Step simpleStep1(@Value("#{jobParameters[requestDate]}") String requestDate) {
-        return stepBuilderFactory.get("simpleStep1")
-                .tasklet((contribution, chunkContext) -> {
-                    log.info("Step1!!!!");
-                    log.info("requestDate = {}", requestDate);
-                    return RepeatStatus.FINISHED;
-                })
-                .build();
+    @StepScope
+    public Tasklet simpleStep2Tasklet(@Value("#{jobParameters[requestDate]}") String requestDate) {
+        return (contribution, chunkContext) -> {
+            log.info("scopeStep1");
+            log.info("requestDate = {}", requestDate);
+            return RepeatStatus.FINISHED;
+        };
     }
 
 }
