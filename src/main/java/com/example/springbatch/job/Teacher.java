cc169051e2fe9383194650b898d995201a8bedac
kyunghwanma <makh1213@naver.com>
2022-12-24 00:08:31 +0900

    Spring Batch ItemProcessor

---
+++ src/main/java/com/example/springbatch/job/Teacher.java
@@ -0,0 +1,24 @@
+package com.example.springbatch.job;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@ToString
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Teacher {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
